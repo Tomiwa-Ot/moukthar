3c555214d72f41be434026465e3abce926ed3ece
Tropes-Ot <ot.server1@outlook.com>
2021-07-27 11:09:53 +0100

    started project

---
+++ AndroidRAT/app/src/main/java/com/ot/androidrat/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ot.androidrat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
