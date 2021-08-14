817b30bb6b05b1c69e30699f0b2259412be3fa8c
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2021-08-14 14:36:02 +0100

    some changes
    password reset, database created,

--- server/__init__.py
+++ server/__init__.py
@@ -1,7 +1,10 @@
+import re
 from flask import Flask, jsonify, render_template, url_for, request, redirect, session
 from flask_socketio import SocketIO, emit
+from passlib.hash import pbkdf2_sha256
+import sqlite3
 
-
+# below dashboard pass data from nav link ro confirm original request
 app = Flask(__name__)
 app.secret_key = 'the rastafarian in you'
 socketio = SocketIO(app)
@@ -31,12 +34,22 @@ def login():
     elif request.method == 'POST' and request.form.get('username') is not None and request.form.get('password') is not None:
         username = request.form.get('username')
         password = request.form.get('password')
-        if username == "admin" and password == "password":
-            session['username'] = username
-            # below dashboard pass data from nav link ro confirm original request logout sqlite
-            return redirect("/", code=302)
-        else:
-            return render_template("login.html", error="Login Failed. Incorrect username/password"), 401
+        try:
+            con = sqlite3.connect("database.db")
+            cur = con.cursor()
+            cur.execute("SELECT * FROM user WHERE username= ?", (username,))
+            record = cur.fetchall()
+            con.close()
+            if record.__len__() != 0 and username == record[0][0]:
+                if pbkdf2_sha256.verify(password, record[0][1]):
+                    session['username'] = username
+                    return redirect("/", code=302)
+                else:
+                    return render_template("login.html", error="Login Failed. Incorrect username/password"), 401    
+            else:
+                return render_template("login.html", error="Login Failed. Incorrect username/password"), 401
+        except sqlite3.Error as error:
+            return render_template("login.html", error="Oops something went wrong"), 401
     else:
         return render_template("error_page.html", code= ["400", "Bad Request"]), 400
 
@@ -85,6 +98,32 @@ def settings():
     else:
         return render_template("error_page.html", code= ["400", "Bad Request"]), 400
 
+@app.route("/reset", methods=['GET', 'POST'])
+def reset_password():
+    if request.method == 'GET' and 'username' in session:
+        return render_template("reset-password.html", username=session['username']), 200
+    elif request.method == 'POST' and request.form.get('username') is not None and request.form.get('password') is not None and 'username' in session:
+        username = request.form.get('username')
+        password = pbkdf2_sha256.hash(request.form.get('password'))
+        try:
+            con = sqlite3.connect("database.db")
+            cur = con.cursor()
+            cur.execute("SELECT * FROM user WHERE username= ?", (session['username'],))
+            record = cur.fetchall()
+            if record.__len__() != 0:
+                cur.execute("INSERT INTO user(username, passkey) VALUES (?, ?)", (username, password))
+                con.commit()
+                con.close()
+                session['username'] = username
+                return redirect("/", 302)
+            else:
+                con.close()
+                return render_template("reset-password.html", error="Oops something went wrong"), 401
+        except sqlite3.Error as error:
+            con.close()
+            return render_template("reset-password.html", error="Oops something went wrong"), 401
+    else:
+        return render_template("error_page.html", code= ["400", "Bad Request"]), 400
 
 @app.errorhandler(404)
 def page_not_found(e):
