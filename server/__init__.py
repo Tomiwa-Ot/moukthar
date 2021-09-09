b64846a3ef168d0779ce0c046047111dead0d1a5
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2021-09-09 09:09:04 +0100

    self signed tls certificate

--- server/__init__.py
+++ server/__init__.py
@@ -220,4 +220,4 @@ def page_not_found(e):
 
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5001, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5001, debug=True, keyfile='key.pem', certfile='cert.pem')
