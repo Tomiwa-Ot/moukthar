2db13479e229f8fa9f7a79bba395b398c3bfff24
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2021-09-09 10:39:33 +0100

    remember me functionality

--- server/__init__.py
+++ server/__init__.py
@@ -1,8 +1,11 @@
 import re
-from flask import Flask, jsonify, render_template, url_for, request, redirect, session
+from sqlite3.dbapi2 import Error
+from flask import Flask, jsonify, render_template, url_for, request, redirect, session, make_response
 from flask_socketio import SocketIO, emit
 from passlib.hash import pbkdf2_sha256
 import sqlite3
+import uuid
+import datetime
 
 # below dashboard pass data from nav link ro confirm original request
 app = Flask(__name__)
@@ -117,6 +120,22 @@ def login():
     if request.method == 'GET':
         if 'username' in session:
             return redirect("/", code=302)
+        elif 'auth' in request.cookies and 'username' in request.cookies:
+            username = request.cookies.get('username')
+            token = request.cookies.get('auth')
+            try:
+                con = sqlite3.connect("database.db")
+                cur = con.cursor()
+                cur.execute("SELECT token FROM user WHERE username= ?", (username,))
+                record = cur.fetchall()
+                con.close()
+                if record.__len__() != 0 and record[0][0] == token:
+                    session['username'] = username
+                    return redirect("/", code=302)
+                else:
+                    return render_template("login.html"), 200
+            except sqlite3.Error as error:
+                pass
         else:
             return render_template("login.html"), 200
     elif request.method == 'POST' and request.form.get('username') is not None and request.form.get('password') is not None:
@@ -131,9 +150,25 @@ def login():
             if record.__len__() != 0 and username == record[0][0]:
                 if pbkdf2_sha256.verify(password, record[0][1]):
                     session['username'] = username
+                    if request.form.get('remember') is not None:
+                        token = str(uuid.uuid4())
+                        try:
+                            con = sqlite3.connect("database.db")
+                            cur = con.cursor()
+                            cur.execute("UPDATE user SET token= ? WHERE username=?", (token, username))
+                            con.commit()
+                            con.close()
+                        except sqlite3.Error as error:
+                            pass
+                        expiry_date = datetime.datetime.now()
+                        expiry_date = expiry_date + datetime.timedelta(days=30)
+                        resp = make_response(redirect("/", code=302))
+                        resp.set_cookie(key='auth', value=token, expires=expiry_date, domain='127.0.0.1')
+                        resp.set_cookie(key='username', value=username, expires=expiry_date, domain='127.0.0.1')
+                        return resp
                     return redirect("/", code=302)
                 else:
-                    return render_template("login.html", error="Login Failed. Incorrect username/password"), 401    
+                    return render_template("login.html", error="Login Failed. Incorrect username/password"), 401
             else:
                 return render_template("login.html", error="Login Failed. Incorrect username/password"), 401
         except sqlite3.Error as error:
@@ -147,6 +182,11 @@ def logout():
     if request.method == 'GET':
         if 'username' in session:
             session.pop('username', None)
+            if 'auth' in request.cookies and 'username' in request.cookies:
+                resp = make_response(redirect("/login", code=302))
+                resp.set_cookie(key='auth', expires=0)
+                resp.set_cookie(key='username', expires=0)
+                return resp
             return redirect("/login", code=302)
         else:
             return redirect("/login", code=302)
@@ -186,6 +226,7 @@ def settings():
     else:
         return render_template("error_page.html", code= ["400", "Bad Request"]), 400
 
+
 @app.route("/reset", methods=['GET', 'POST'])
 def reset_password():
     if request.method == 'GET' and 'username' in session:
@@ -193,16 +234,25 @@ def reset_password():
     elif request.method == 'POST' and request.form.get('username') is not None and request.form.get('password') is not None and 'username' in session:
         username = request.form.get('username')
         password = pbkdf2_sha256.hash(request.form.get('password'))
+        token = str(uuid.uuid4())
         try:
             con = sqlite3.connect("database.db")
             cur = con.cursor()
             cur.execute("SELECT * FROM user WHERE username= ?", (session['username'],))
             record = cur.fetchall()
             if record.__len__() != 0:
-                cur.execute("UPDATE user SET username= ?, passkey= ? WHERE username=?", (username, password, session['username']))
+                cur.execute("UPDATE user SET username= ?, passkey= ?, token= ? WHERE username=?", (username, password, token, session['username']))
                 con.commit()
                 con.close()
                 session['username'] = username
+                if 'auth' in request.cookies and 'username' in request.cookies:
+                    resp = make_response(redirect("/", 302))
+                    expiry_date = datetime.datetime.now()
+                    expiry_date = expiry_date + datetime.timedelta(days=30)
+                    resp = make_response(redirect("/", code=302))
+                    resp.set_cookie(key='auth', value=token, expires=expiry_date, domain='127.0.0.1')
+                    resp.set_cookie(key='username', value=username, expires=expiry_date, domain='127.0.0.1')
+                    return resp
                 return redirect("/", 302)
             else:
                 con.close()
