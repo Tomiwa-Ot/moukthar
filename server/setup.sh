68fb311c9703c2fbeac8ad8d44fb5d9a2c69cb37
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2021-09-11 22:37:32 +0100

    update setup

--- server/setup.sh
+++ server/setup.sh
@@ -2,5 +2,7 @@
 
 # Tested on Ubuntu
 sudo mv moukthar.service /etc/systemd/system/
+sudo systemctl start nginx
+sudo systemctl enable nginx
 sudo systemctl enable moukthar.service
 sudo systemctl start moukthar.service
