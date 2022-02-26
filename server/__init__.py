ab62b2d4dfa85f3e4393fccb3631a9a48752c281
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2022-02-26 10:31:20 +0100

    Update __init__.py

--- server/__init__.py
+++ server/__init__.py
@@ -29,25 +29,6 @@ socketio = SocketIO(app)
 0x10 - Factory Reset Device
 '''
 
-commands = [
-    "0x0",
-    "0x1",
-    "0x2",
-    "0x3",
-    "0x4",
-    "0x5",
-    "0x6",
-    "0x7",
-    "0x8",
-    "0x9",
-    "0xA",
-    "0xB",
-    "0xC",
-    "0xD",
-    "0xE",
-    "0x10",
-]
-
 
 # Connection from C2
 @socketio.on('connect')
