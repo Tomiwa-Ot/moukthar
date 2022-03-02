420bb767f155c67519db0ae894903e2e0cfe21b3
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2022-03-02 19:50:23 +0100

    get files handler

--- server/static/assets/js/files.js
+++ server/static/assets/js/files.js
@@ -1,26 +1,63 @@
 $(document).ready(function (){
-    // on a click get text post to files
-    // provide forward and backward navigation linked list??
 
-    $('#btn-left-arrow').click(function (){
+    var history = []
+    var currentPage = -1
+    var directory = ["/"]
+
+    $.ajax({
+        url: '/get-files',
+        type: 'post',
+        data: {
+            'path' : '/'
+        }
+        success: function(data) {
+            $('.files-class').append(data.htmlresponse);
+        }
+    })
+
+    $('.file-link').click(function (){
+        var path = $(this).text()
         $.ajax({
             url: '/get-files',
             type: 'post',
-            data: {}
-            success: function(data){
-                
+            data: {
+                'path' : `${path}`
+            }
+            success: function(data) {
+                currentPage++
+                $('.files-class').html(data.htmlresponse);
             }
         })
     })
 
+    $('#btn-left-arrow').click(function (){
+        if (history.length != 0 && currentPage != -1) {
+            $.ajax({
+                url: '/get-files',
+                type: 'post',
+                data: {
+                    'path' : history[--currentPage]
+                }
+                success: function(data) {
+                    currentPage++
+                    $('.files-class').html(data.htmlresponse);
+                }
+            })
+        }
+    })
+
     $('#btn-right-arrow').click(function (){
-        $.ajax({
-            url: '/get-files',
-            type: 'post',
-            data: {}
-            success: function(data){
-                
-            }
-        });
-    });
+        if (history.length != 0 && currentPage != history.length) {
+            $.ajax({
+                url: '/get-files',
+                type: 'post',
+                data: {
+                    'path' : history[++currentPage]
+                }
+                success: function(data) {
+                    $('.files-class').html(data.htmlresponse);
+                }
+            })
+        }
+    })
 })
