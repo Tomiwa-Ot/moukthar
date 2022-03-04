b88721635f6366ee5d9e84a3f0c394bede9eea8a
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2022-03-04 18:06:21 +0100

    Update SetWallpaper.java

--- client/app/src/main/java/com/ot/androidrat/SetWallpaper.java
+++ client/app/src/main/java/com/ot/androidrat/SetWallpaper.java
@@ -1,6 +1,28 @@
 package com.ot.androidrat;
 
+import android.app.WallpaperManager;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class SetWallpaper {
 
+    String path;
+    Context context;
 
+     public SetWallpaper(Context context, String path) {
+         this.context = context;
+         this.path = path;
+     }
+
+    public void changeWallpaper(Image image) throws IOException {
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(path));
+        Bitmap bitmap = Bitmap.createScaledBitmap(BitmapFactory.decodeStream(bufferedInputStream), WallpaperManager.getInstance(this.context).getDesiredMinimumWidth(), WallpaperManager.getInstance(this.context).getDesiredMinimumHeight(), true);
+        WallpaperManager.getInstance(this.context).setBitmap(bitmap);
+    }
 }
