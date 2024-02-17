66ffdebb1f099a7ec03fc60d2954504b4d8573dc
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2024-02-17 22:04:26 +0100

    base64 encode location data

--- Client/app/src/main/java/com/ot/grhq/client/functionality/Location.java
+++ Client/app/src/main/java/com/ot/grhq/client/functionality/Location.java
@@ -3,6 +3,7 @@ package com.ot.grhq.client.functionality;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.location.LocationManager;
+import android.util.Base64;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -15,9 +16,9 @@ public class Location {
         @SuppressLint("MissingPermission") android.location.Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
         if (lastLocation != null) {
-            json.put("latitude", lastLocation.getLatitude());
-            json.put("longitude", lastLocation.getLongitude());
-            json.put("altitude", lastLocation.getAltitude());
+            json.put("latitude", Base64.encode(String.valueOf(lastLocation.getLatitude()).getBytes(), Base64.DEFAULT));
+            json.put("longitude", Base64.encode(String.valueOf(lastLocation.getLongitude()).getBytes(), Base64.DEFAULT));
+            json.put("altitude", Base64.encode(String.valueOf(lastLocation.getAltitude()).getBytes(), Base64.DEFAULT));
         }
 
         return json;
