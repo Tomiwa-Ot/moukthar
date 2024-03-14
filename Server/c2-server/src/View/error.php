fb8f2852ab093d16551ced03f5963737302dbc13
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2024-03-14 22:11:41 +0100

    made additions

--- Server/c2-server/src/View/error.php
+++ Server/c2-server/src/View/error.php
@@ -1,3 +1,7 @@
 <?php require __DIR__ . "/header.php"; ?>
-
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 align-items-center"><h1>¯\_(ツ)_/¯</h1></div>
+    </div>
+</div>
 <?php require __DIR__ . "/footer.php"; ?>
